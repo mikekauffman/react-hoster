5a40d700568ed2e4ea896087b0d41342761c6832
Mike Kauffman <mkauffman@quickleft.com>
2016-03-02 16:37:48 -0700

    Styling improvements

--- components/hoster.js
+++ components/hoster.js
@@ -5,7 +5,7 @@ export default class Hoster extends React.Component {
   render() {
     return(
       <div>
-        <h1>Hoster</h1>
+        <h1 className='text-center col-xs-12'>Hoster</h1>
         <PartyForm/>
       </div>
     )
