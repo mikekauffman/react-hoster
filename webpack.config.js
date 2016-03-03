62c41948b5b2529749238a5092de3325f911265a
Mike Kauffman <mkauffman@quickleft.com>
2016-03-03 10:58:25 -0700

    Adding babel loader back into web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,15 @@ module.exports = {
   module: {
     loaders: [
       {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: ["syntax-object-rest-spread", "transform-object-rest-spread"]
+        }
+      },
+      {
         test: /\.css$/, loader: "style!css"
       },
       {
